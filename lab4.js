b7689f339052c8da24786e3ca529a2a61ee38d45
mkokkatt <47791277+mkokkatt@users.noreply.github.com>
2020-10-07 02:37:42 -0400

    Add files via upload

---
+++ lab4.js
@@ -0,0 +1,19 @@
+
+
+
+
+function iterate(node) {
+		alert(node.children[0].children[1].children[0]);
+	
+
+}
+
+
+iterate(document.getRootNode());
+
+
+
+
+//html element is document.getRootNode().children[0]
+
+//<!doctype html>
