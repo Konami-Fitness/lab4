288638ec20a47f6c4b0681304cbf9e33ec2ba8ef
teddy-clark <71199884+teddy-clark@users.noreply.github.com>
2020-10-13 02:03:51 -0400

    commented and condensed lab4_1b.js

--- lab4_1b.js
+++ lab4_1b.js
@@ -1,90 +1,63 @@
-
-function iterate(node, total) {
-
+function iterate(node, string_lvl) {
 	var children = node.childNodes;
 	// children contains the child nodes of the current node element
-
-	if(total.level == 0) {
+	if(string_lvl.level == 0) {
 		// if on the first level -> print tagName, which is just the html tag
-		total.str.push('-'.repeat(total.level) + node.tagName + "\n");
-		total.level++;
+		string_lvl.str_arr.push('-'.repeat(string_lvl.level) + node.tagName + "\n");
+		string_lvl.level++;
 	}
-
-
 	var elements = document.getElementsByClassName(node.tagName.toLowerCase());
-
+	// elements have a class name of the parent element tag
 
 	if(node.tagName.toLowerCase() == "div") {
-
-	for(x in children) {
-		// for each of the child nodes print dashes times the depth level and its tag name
-		if(children[x].nodeType === 1) {
-			// if the node is an element node
-			total.str.push('-'.repeat(total.level) + children[x].tagName + "\n");
-			total.level++;
-			// increase level by one before the recursive call to add a layer of depth
-			iterate(children[x], total);
-			// decrease level by one after the recursive call to subtract a layer of depth
-			total.level--;
-
-
-		}
-	}
-
-
-
-	} else {
-
-	for(y in elements) {
-
-			if(elements[y].nodeType === 1) {
-			// if the node is an element node
-				total.str.push('-'.repeat(total.level) + elements[y].tagName + "\n");
-				total.level++;
-			// increase level by one before the recursive call to add a layer of depth
-				iterate(elements[y], total);
-			// decrease level by one after the recursive call to subtract a layer of depth
-				total.level--;
-
-
+		for(x in children) {
+			// for each of the child nodes in the div tags, print the tag name and depth recursively
+			if(children[x].nodeType === 1) {
+				// if the node is an element node
+				string_lvl.str_arr.push('-'.repeat(string_lvl.level) + children[x].tagName + "\n");
+				string_lvl.level++;
+				// increase level by one before the recursive call to add a layer of depth
+				iterate(children[x], string_lvl);
+				string_lvl.level--;
+				// decrease level by one after the recursive call to subtract a layer of depth
 			}
-
 		}
-
-
-
+	} else {
+		for(y in elements) {
+			// prints elements based off the class name
+			if(elements[y].nodeType === 1) {
+				// if the node is an element node
+				string_lvl.str_arr.push('-'.repeat(string_lvl.level) + elements[y].tagName + "\n");
+				string_lvl.level++;
+				// increase level by one before the recursive call to add a layer of depth
+				iterate(elements[y], string_lvl);
+				string_lvl.level--;
+				// decrease level by one after the recursive call to subtract a layer of depth
+			}
+		}
 	}
-
-
-
-	return total.str;
+	return string_lvl.str_arr;
 }
-var object1 = {
-	str: [], // return array
+
+var string_lvl = {
+	str_arr: [], // return array
 	level: 0 // level 0 starts with the html element
 };
 
-var result = iterate(document.getRootNode().children[0], object1);
+var tag_lst = iterate(document.getRootNode().children[0], object1);
+// tag_lst contains an array of the tags in the document
 
 var para = document.createElement("P");
 para.setAttribute("id", "part1b");
-/*
-para.appendChild(document.createTextNode(result[0]));
-para.appendChild(temp)
-para.appendChild(document.createTextNode(result[1]));
-document.body.appendChild(para);
-*/
-for(g in result) {
+// create a new p element and set its ID to part1b
 
-var txt = document.createTextNode(result[g]);
-para.appendChild(txt);
-var temp = document.createElement("br");
-
-para.appendChild(temp);
+for(g in tag_lst) {
+	// for each tag, add the the tag to text node followed by br tag and add both
+	// to a paragraph tag to presevere printing
+	var txt = document.createTextNode(tag_lst[g]);
+	para.appendChild(txt);
+	var temp = document.createElement("br");
+	para.appendChild(temp);
 }
-
 document.body.appendChild(para);
-
-
-//html element is document.getRootNode().children[0]
-
+// add the paragraph tag to the end of the body tag
