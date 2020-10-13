55fd3c2516cb1a52f21ff448553f6cef1e7812bf
shaneriggs <61204126+shaneriggs@users.noreply.github.com>
2020-10-13 01:32:48 -0400

    Add files via upload

---
+++ lab4_3.js
@@ -0,0 +1,27 @@
+// cloneNode() and appendChild()
+var itm = document.getElementsByClassName("quote"); // array of all "quote" class items
+var last = itm[itm.length-1] // take the last item from the array
+var cln = last.cloneNode(true); // clone the last one
+cln.innerHTML = "'If you've got a problem with Canada Gooses, you've got a problem with me. And I suggest you let that one marinate.' - Wayne from Letterkenny"; // replace the text with the quote
+document.getElementById("body").appendChild(cln); // append the quote onto the end of the body
+
+
+
+
+// onmouseover and onmouseout
+// creates an array of all of the divs
+var element = document.getElementsByTagName("div");
+
+var i;
+//iterates through the divs, updates each one with the 
+// new onmouseover and onmouseout attributes
+for (i = 0; i < element.length; i++) {
+	element[i].onmouseover = function() {
+			this.style.backgroundColor = "red";
+			this.style.marginLeft = "10px";
+		};
+	element[i].onmouseout = function() {
+			this.style.backgroundColor = "white";
+			this.style.marginLeft = "0px";
+		};
+}
